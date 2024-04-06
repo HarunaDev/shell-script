d903abb545095e959f8ecc1bdcd574e1eb5cc584
Alvin Haruna <harunaalvin69@gmail.com>
2024-04-06 08:02:50 +0100

    Added shell script that creates a flask project with a virtual environment

---
+++ create-flask-project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "This script creates a flask-project directory, it takes one argument variable, which is the name of the project folder."
+cd Desktop/Projects/ && mkdir $1 && cd $1 && git init && code .
+touch .gitignore && echo "env/" >
+Python -m venv env && source env/Scripts/activate
+pip install flask && mkdir templates && cd templates && touch base.html index.html
+cd ../ && touch app.py REQUIREMENTS.txt README.md NOTES.md && echo "from flask import Flask, render_template, request" > app.py
+git add * && git commit -m "initial commit for flask project"
